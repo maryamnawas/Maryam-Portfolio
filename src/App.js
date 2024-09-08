b4fc2bf61fe08961effbc91ec9176c07d89dcd46
maryamnawas <125673565+maryamnawas@users.noreply.github.com>
2024-09-08 20:37:27 +0530

    created all the components for the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import Recommandation from "./components/Recommandation/Recommandation";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Header/>
+      <About/>
+      <Services/>
+      <Skills/>
+      <Projects/>
+      <Recommandation/>
+      <Contact/>
+      <Footer/>
     </>
   );
 }
